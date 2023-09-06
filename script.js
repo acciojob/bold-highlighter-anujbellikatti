5f61c3d12bf7271045dcebc6f8d034e1b2758749
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-09-07 00:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function highlight() {
     //Write your code here
+  var boldElements = document.querySelectorAll("strong");
 
+    // Loop through the <strong> elements and change their color to green
+    for (var i = 0; i < boldElements.length; i++) {
+        boldElements[i].style.color = "green";
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
+ var boldElements = document.querySelectorAll("strong");
 
+    // Loop through the <strong> elements and change their color back to black
+    for (var i = 0; i < boldElements.length; i++) {
+        boldElements[i].style.color = "black";
+    }
     
 }
+
